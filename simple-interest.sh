27653488b0c7f95896e990743d463c31984b59fd
Deepshikha Chaudhary <chaudharydeepshikha4@gmail.com>
2025-03-16 18:10:37 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+```sh
+#!/bin/bash
+
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <principal> <rate> <time>"
+    exit 1
+fi
+
+principal=$1
+rate=$2
+time=$3
+
+interest=$(echo "$principal * $rate * $time / 100" | bc)
+
+echo "Simple Interest: $interest"
